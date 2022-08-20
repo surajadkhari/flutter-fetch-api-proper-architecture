b6865489ceb00943b1a3109f95c672b5ef2fc192
Surat Adhikari <surajadkhari@gmail.com>
2022-08-20 23:39:40 +0545

    #5 presentation : UI

--- lib/features/presentation/widgets/home.dart
+++ lib/features/presentation/widgets/home.dart
@@ -1,10 +1,30 @@
+import 'package:fetch_api_with_proper_architecure/features/presentation/controllers/post_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final data = ref.watch(postControllerProvider);
+    return Scaffold(
+      body: data.when(
+          data: (data) {
+            return ListView(
+              children: [
+                ...data.map((e) => ListTile(
+                      contentPadding: const EdgeInsets.all(20),
+                      title: Text(
+                        "${e.firstName} ${e.lastName}",
+                      ),
+                      trailing: Image.network(e.avatar),
+                    ))
+              ],
+            );
+          },
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: () => const CircularProgressIndicator()),
+    );
   }
 }
