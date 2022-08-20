a2f8842b74854d2a7b4a63f0a5346c82c6984c90
Surat Adhikari <surajadkhari@gmail.com>
2022-08-20 20:52:11 +0545

    api_client that can works for both get and post method

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -1,3 +1,24 @@
-class ApiClient{
-  
+import 'package:dio/dio.dart';
+import 'package:fetch_api_with_proper_architecure/core/api_const.dart';
+
+class ApiClient {
+  Future request(
+      {required String path,
+      required String method,
+      bool isFormData = false,
+      Map<String, dynamic> data = const {}}) async {
+    Dio dio = Dio(BaseOptions(baseUrl: ApiConstant.baseUrl, headers: {
+      'Content-Type': 'application/json',
+      "Accept": 'application/json',
+    }));
+    try {
+      final response = method == "get"
+          ? await dio.get(path)
+          : await dio.post(path,
+              data: isFormData ? FormData.fromMap(data) : data);
+      return response.data;
+    } on DioError catch (e) {
+      throw Exception(e);
+    }
+  }
 }
