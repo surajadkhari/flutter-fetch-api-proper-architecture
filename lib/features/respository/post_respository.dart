ddaaa75f931d0858502da062fbcd6447b483f3c1
Surat Adhikari <surajadkhari@gmail.com>
2022-08-20 22:43:11 +0545

    #3 domain: repo implementation method

--- lib/features/respository/post_respository.dart
+++ lib/features/respository/post_respository.dart
@@ -1,6 +1,23 @@
 import 'package:dartz/dartz.dart';
+import 'package:fetch_api_with_proper_architecure/core/api_exception.dart';
+import 'package:fetch_api_with_proper_architecure/features/data/data_sources/post_data_source.dart';
 import 'package:fetch_api_with_proper_architecure/features/data/models/post_response_model.dart';
+import '../../core/app_error.dart';
 
-abstract class PostReposiotory {
-  Future<Either<String, List<PostResponseModel>>> getPost();
+abstract class PostRepository {
+  Future<Either<AppError, List<PostResponseModel>>> getPost();
+}
+
+class PostRepositoryImpl implements PostRepository {
+  final PostDataSource _postDataSource;
+  PostRepositoryImpl(this._postDataSource);
+  @override
+  Future<Either<AppError, List<PostResponseModel>>> getPost() async {
+    try {
+      final result = await _postDataSource.getPost();
+      return Right(result);
+    } on DioException catch (e) {
+      return Left(AppError(e.message!));
+    }
+  }
 }
