172e79dd8722ec9de19034101838b9e0fd09fac7
Surat Adhikari <surajadkhari@gmail.com>
2022-08-21 00:01:49 +0545

    #5 ui: use asyncValue widget

--- lib/features/presentation/widgets/home.dart
+++ lib/features/presentation/widgets/home.dart
@@ -1,4 +1,6 @@
+import 'package:fetch_api_with_proper_architecure/features/data/models/post_response_model.dart';
 import 'package:fetch_api_with_proper_architecure/features/presentation/controllers/post_controller.dart';
+import 'package:fetch_api_with_proper_architecure/utils/asyncvalue_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,25 +9,43 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(postControllerProvider);
+    final result = ref.watch(postControllerProvider);
     return Scaffold(
-      body: 
-      data.when(
-          data: (data) {
-            return ListView(
-              children: [
-                ...data.map((e) => ListTile(
-                      contentPadding: const EdgeInsets.all(20),
-                      title: Text(
-                        "${e.firstName} ${e.lastName}",
-                      ),
-                      trailing: Image.network(e.avatar),
-                    ))
-              ],
-            );
-          },
-          error: (error, stackTrace) => Text(error.toString()),
-          loading: () => const CircularProgressIndicator()),
-    );
+        body: AsyncValueWidget<List<PostResponseModel>>(
+      value: result,
+      data: (data) {
+        return ListView(
+          children: [
+            ...data.map((e) => ListTile(
+                  contentPadding: const EdgeInsets.all(20),
+                  title: Text(
+                    "${e.firstName} ${e.lastName}",
+                  ),
+                  trailing: Image.network(e.avatar),
+                ))
+          ],
+        );
+      },
+    )
+        // data.when(
+        //   data: (data) {
+        //     return ListView(
+        //       children: [
+        //         ...data.map((e) => ListTile(
+        //               contentPadding: const EdgeInsets.all(20),
+        //               title: Text(
+        //                 "${e.firstName} ${e.lastName}",
+        //               ),
+        //               trailing: Image.network(e.avatar),
+        //             ))
+        //       ],
+        //     );
+        //   },
+        //   error: (error, stackTrace) => Text(
+        //     error.toString(),
+        //   ),
+        //   loading: () => const CircularProgressIndicator(),
+        // ),
+        );
   }
 }
--- lib/utils/asyncvalue_widget.dart
+++ lib/utils/asyncvalue_widget.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AsyncValueWidget extends StatelessWidget {
+class AsyncValueWidget<T> extends StatelessWidget {
   const AsyncValueWidget({Key? key, required this.value, required this.data})
       : super(key: key);
-  final AsyncValue value;
-  final Widget Function(dynamic) data;
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
   @override
   Widget build(BuildContext context) {
     return Consumer(
